ec83723985988650e6baa93c9e1c5cec9456193c
Oli Wennell <oli.wennell@gmail.com>
2019-07-16 14:57:05 +0100

    :construction_worker: Build fails if unsufficient test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     setupFiles: [
         "./tools/enzymeSetup.js"
-    ]
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100
+          }
+    }
 };
