590d20500145d41849b097883e1e0e7c373df5f0
Oli Wennell <oli.wennell@gmail.com>
2019-07-16 16:28:22 +0100

    :rocket: Using github pages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Game from "./components/Game";
-import "./index.css"
-
-const startTimer = callback => setInterval(callback, 250);
-
-render(<Game startTimerFn={startTimer} />, document.getElementById("root"));
